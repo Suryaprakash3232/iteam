398fcf6501aaae99c7f0ad68204607af354b6bc8
Suryaprakash3232 <suryaprakash63v@gmail.com>
2024-09-09 22:54:19 +0530

    Add files via upload

---
+++ java.java
@@ -0,0 +1,10 @@
+public class java {
+    public static void main(String[] args) {
+        System.out.println("first Line");
+        System.out.print("second Line");
+        System.out.println("third Line");
+        System.out.print("fourth Line");
+
+
+    }
+}
